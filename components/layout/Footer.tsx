a272cfbdfc160b38581bfd76a07fc33ff6b795c3
dev-rome <haynesjerome@gmail.com>
2023-07-13 13:58:27 -0400

    add the Footer component to render the footer section of the application

---
+++ components/layout/Footer.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Image from "next/image";
+import Logo from "@/public/images/logo.svg";
+import { links } from "@/constants/navigation";
+import NavLinks from "./NavLinks";
+
+const Footer = () => {
+  return (
+    <footer>
+      <Image src={Logo} alt="Little Lemon" width={175} />
+      <div>
+        <h3>Doormat Navigation</h3>
+        <ul>
+          {links.map(({ text, href }) => (
+            <NavLinks key={text} text={text} href={href} />
+          ))}
+        </ul>
+      </div>
+
+      <div>
+        <h3>Contact</h3>
+        <p>Address</p>
+        <p>phone number</p>
+        <p>email</p>
+      </div>
+
+      <div>
+        <h3>Social Media</h3>
+        <li>
+          <a href=""></a>
+        </li>
+        <li>
+          <a href=""></a>
+        </li>
+        <li>
+          <a href=""></a>
+        </li>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
