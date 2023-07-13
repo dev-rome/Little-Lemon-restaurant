cee679413daf9e147aee9762009718537bf94cd2
dev-rome <haynesjerome@gmail.com>
2023-07-13 13:54:13 -0400

    added the  folder, which contains files for storing reusable constants and configurations

---
+++ constants/navigation.ts
@@ -0,0 +1,26 @@
+export const links = [
+  {
+    text: "Home",
+    href: "/",
+  },
+  {
+    text: "About",
+    href: "/about",
+  },
+  {
+    text: "Menu",
+    href: "/menu",
+  },
+  {
+    text: "Reservations",
+    href: "/reservations",
+  },
+  {
+    text: "Order Online",
+    href: "/order online",
+  },
+  {
+    text: "Login",
+    href: "/login",
+  },
+];
