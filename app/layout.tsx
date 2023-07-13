9d9854340df0df1cd1252c0fc0c872a8b73f7f2b
dev-rome <haynesjerome@gmail.com>
2023-07-13 13:56:15 -0400

    add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,29 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Little Lemon Restaurant',
-}
+  title: "Little Lemon Restaurant",
+  description:
+    "Little Lemon is a restaurant that serves the best food in the world. We're located in the heart of the city.",
+  openGraph: {
+    title: "Little Lemon Restaurant",
+    description:
+      "Little Lemon is a restaurant that serves the best food in the world. We're located in the heart of the city.",
+    type: "website",
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
