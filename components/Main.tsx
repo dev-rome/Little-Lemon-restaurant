28896d355a631f946774a0c0072c0e11407a1d61
dev-rome <haynesjerome@gmail.com>
2023-07-13 13:54:41 -0400

    add main page component

---
+++ components/Main.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Nav from "./layout/Nav";
+import Footer from "./layout/Footer";
+
+const Main = () => {
+  return (
+    <>
+      <Nav />
+
+      <Footer />
+    </>
+  );
+};
+
+export default Main;
