5db49803f8bd782660cfd2f7ca575727c4d611fe
dev-rome <haynesjerome@gmail.com>
2023-07-13 21:53:10 -0400

    improve mobile navigation animation and component structure

--- components/layout/Nav.tsx
+++ components/layout/Nav.tsx
@@ -10,11 +10,34 @@ import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 import Logo from "@/public/images/logo.svg";
 
+import { MobileNavProps } from "@/types";
+
+const MobileNav = ({ isOpen, closeNav }: MobileNavProps) => {
+  return (
+    <div
+      className={`fixed right-0 top-0 bottom-0 w-64 bg-[#495E57] text-white z-50 transform transition-transform duration-300 ${
+        isOpen ? "translate-x-0" : "translate-x-full"
+      } lg:hidden`}
+    >
+      <div className="flex justify-end p-4">
+        <AiOutlineClose
+          className="text-3xl cursor-pointer"
+          onClick={closeNav}
+        />
+      </div>
+      <ul className="flex flex-col gap-4 p-4">
+        {links.map(({ text, href }) => (
+          <NavLinks key={text} text={text} href={href} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
 const Nav = () => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const toggleNav = () => setIsOpen(!isOpen);
-  const closeNav = () => setIsOpen(false);
+  const toggleNav = () => setIsOpen((prevIsOpen) => !prevIsOpen);
 
   return (
     <nav className="p-4">
@@ -26,34 +49,25 @@ const Nav = () => {
           className="object-contain"
         />
         <div className="lg:hidden">
-          <AiOutlineMenu
-            className="text-3xl cursor-pointer"
-            onClick={toggleNav}
-          />
+          {isOpen ? (
+            <AiOutlineClose
+              className="text-3xl cursor-pointer"
+              onClick={toggleNav}
+            />
+          ) : (
+            <AiOutlineMenu
+              className="text-3xl cursor-pointer"
+              onClick={toggleNav}
+            />
+          )}
         </div>
-        <ul className="hidden lg:flex gap-11">
+        <ul className={`hidden lg:flex gap-11 ${isOpen ? "lg:hidden" : ""}`}>
           {links.map(({ text, href }) => (
             <NavLinks key={text} text={text} href={href} />
           ))}
         </ul>
       </div>
-      <div
-        className={`fixed right-0 top-0 bottom-0 w-64 bg-[#495E57] text-white z-50 transform transition-transform duration-300 ${
-          isOpen ? "translate-x-0" : "translate-x-full"
-        } lg:hidden`}
-      >
-        <div className="flex justify-end p-4">
-          <AiOutlineClose
-            className="text-3xl cursor-pointer"
-            onClick={closeNav}
-          />
-        </div>
-        <ul className="flex flex-col gap-4 p-4">
-          {links.map(({ text, href }) => (
-            <NavLinks key={text} text={text} href={href} />
-          ))}
-        </ul>
-      </div>
+      <MobileNav isOpen={isOpen} closeNav={toggleNav} />
     </nav>
   );
 };
