a3581d02dc7ad8ebeff17899041f59d46b439b79
dev-rome <haynesjerome@gmail.com>
2023-07-13 21:20:16 -0400

    edit header layout, import, and added header content component

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -2,9 +2,45 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { karla, markaziText } from "@/app/fonts";
-import HeaderImage from "@/public/images/restauranfood.jpg";
+
 import Nav from "@/components/layout/Nav";
 import Button from "../Button";
+
+import HeaderImage from "@/public/images/restauranfood.jpg";
+
+const HeaderContent = () => {
+  return (
+    <div className="text-white">
+      <div className="flex flex-col md:flex-row md:justify-evenly gap-5">
+        <div>
+          <h1 className={`text-56 ${markaziText.className} font-bold`}>
+            Little Lemon
+          </h1>
+          <span className={`text-32 ${markaziText.className}`}>New York</span>
+          <p
+            className={`max-w-2xl md:max-w-lg text-20 ${karla.className} mb-5`}
+          >
+            Experience culinary bliss at Little Lemon, where taste meets
+            artistry. Immerse yourself in a world of exquisite flavors,
+            carefully crafted from the finest ingredients.
+          </p>
+          <Button className="text-18 bg-primary-yellow text-black rounded px-6 py-3 mb-10">
+            <Link href="/reserve">Reserve a Table</Link>
+          </Button>
+        </div>
+        <div className="self-center">
+          <Image
+            src={HeaderImage}
+            alt="restaurant food being served"
+            width={300}
+            height={100}
+            className="object-contain rounded-2xl"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const Header = () => {
   return (
@@ -12,34 +48,7 @@ const Header = () => {
       <Nav />
       <div className="bg-primary-darkGreen h-[75vh] md:h-[50vh]">
         <div className="max-w-[80rem] mx-auto p-4">
-          <div className="flex flex-col md:flex-row md:justify-evenly gap-5">
-            <div className="text-white">
-              <h1 className={`text-56 ${markaziText.className} font-bold`}>
-                Little Lemon
-              </h1>
-              <span className={`text-32 ${markaziText.className}`}>
-                New York
-              </span>
-              <p
-                className={`max-w-2xl md:max-w-lg text-20 ${karla.className} mb-5`}
-              >
-                Experience culinary bliss at Little Lemon, where taste meets
-                artistry. Immerse yourself in a world of exquisite flavors,
-                carefully crafted from the finest ingredients.
-              </p>
-              <Button className="text-18 bg-primary-yellow text-black rounded px-6 py-3 mb-10">
-                <Link href="/reserve">Reserve a Table</Link>
-              </Button>
-            </div>
-
-            <Image
-              src={HeaderImage}
-              alt="restaurant food being served"
-              width={300}
-              height={100}
-              className="object-contain rounded-2xl self-center"
-            />
-          </div>
+          <HeaderContent />
         </div>
       </div>
     </header>
