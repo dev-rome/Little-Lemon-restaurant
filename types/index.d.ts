4a2831e4e872200e477db80c4bbdf34cdb532177
dev-rome <haynesjerome@gmail.com>
2023-07-14 16:49:56 -0400

    add card and special props

--- types/index.d.ts
+++ types/index.d.ts
@@ -12,3 +12,15 @@ export interface ButtonProps {
   children: React.ReactNode;
   className?: string;
 }
+
+export interface CardProps {
+  special: Special;
+}
+
+export interface SpecialProps {
+  id: string;
+  image: string;
+  title: string;
+  price: string;
+  description: string;
+}
