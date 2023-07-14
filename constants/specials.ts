8c57440144531a0e686eafc1b73cc69c17ddce4f
dev-rome <haynesjerome@gmail.com>
2023-07-14 16:50:17 -0400

    add specials data

---
+++ constants/specials.ts
@@ -0,0 +1,26 @@
+export const specialsData = [
+  {
+    id: 1,
+    image: "/images/greek salad.jpg",
+    title: "Greek salad",
+    price: "$12.99",
+    description:
+      "The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. ",
+  },
+  {
+    id: 2,
+    image: "/images/bruchetta.jpg",
+    title: "Bruchetta",
+    price: "$5.99",
+    description:
+      "Our Bruchetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil. ",
+  },
+  {
+    id: 3,
+    image: "/images/lemon dessert.jpg",
+    title: "Lemon Dessert",
+    price: "$5.00",
+    description:
+      "This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined.",
+  },
+];
