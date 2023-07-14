8c946bc612642984febd0aa3d1b0a4054b1deed6
dev-rome <haynesjerome@gmail.com>
2023-07-13 20:21:08 -0400

    removed props and added children props

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,24 +1,15 @@
 import React from "react";
-import classnames from "classnames";
 import { ButtonProps } from "@/types";
 
 const Button = ({
-  text,
+  children,
   className,
-  reserveTable,
-  orderMenu,
-  orderDelivery,
   ...rest
 }: ButtonProps) => {
-  const buttonClasses = classnames({
-    "text-18 bg-#000 text-white rounded px-6 py-5": reserveTable || orderMenu,
-    "bg-none text-16, text-darkGray": orderDelivery,
-    className,
-  });
 
   return (
-    <button className={buttonClasses} {...rest}>
-      {text}
+    <button className={className} {...rest}>
+      {children}
     </button>
   );
 };
