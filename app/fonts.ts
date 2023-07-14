50412de27b139aeb896737a3ba987432cdc98291
dev-rome <haynesjerome@gmail.com>
2023-07-13 20:23:13 -0400

    removed weight from font file

---
+++ app/fonts.ts
@@ -0,0 +1,11 @@
+import { Karla, Markazi_Text } from "next/font/google";
+
+export const karla = Karla({
+  style: "normal",
+  subsets: ["latin"],
+});
+
+export const markaziText = Markazi_Text({
+  style: "normal",
+  subsets: ["latin"],
+});
