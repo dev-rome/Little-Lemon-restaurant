9eff5c87b23717138e83302bc3e8576312403013
dev-rome <haynesjerome@gmail.com>
2023-07-13 20:25:22 -0400

    removed nav and footer, added header

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,13 +1,10 @@
 import React from "react";
-import Nav from "./layout/Nav";
-import Footer from "./layout/Footer";
+import Header from "./layout/Header";
 
 const Main = () => {
   return (
     <>
-      <Nav />
-
-      <Footer />
+      <Header />
     </>
   );
 };
