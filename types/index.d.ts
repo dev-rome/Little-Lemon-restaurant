7b4b4351937185db645ede5439ab07d706cd0a9f
dev-rome <haynesjerome@gmail.com>
2023-07-13 21:51:19 -0400

    add mobile nav props

--- types/index.d.ts
+++ types/index.d.ts
@@ -3,6 +3,11 @@ export interface NavLinkProps {
   href: string;
 }
 
+interface MobileNavProps {
+  isOpen: boolean;
+  closeNav: () => void;
+}
+
 export interface ButtonProps {
   children: React.ReactNode;
   className?: string;
