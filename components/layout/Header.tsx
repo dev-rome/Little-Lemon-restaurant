0360f6b2c03744ee4525f6d07a2b148d7f62bcbb
dev-rome <haynesjerome@gmail.com>
2023-07-14 17:46:30 -0400

    added css grid

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -10,31 +10,35 @@ const Header = () => {
   return (
     <header className="bg-primary-darkGreen py-10">
       <div className="max-w-[80rem] mx-auto px-4 ">
-        <div className="flex flex-col justify-between md:flex-row md:justify-evenly items-center gap-5">
-          <div className="text-white">
-            <h1 className={`text-56 ${markaziText.className} font-bold`}>
-              Little Lemon
-            </h1>
-            <span className={`text-32 ${markaziText.className}`}>New York</span>
-            <p
-              className={`max-w-2xl md:max-w-lg text-20 ${karla.className} mb-5`}
-            >
-              Experience culinary bliss at Little Lemon, where taste meets
-              artistry. Immerse yourself in a world of exquisite flavors,
-              carefully crafted from the finest ingredients.
-            </p>
-            <Button className="text-18 bg-primary-yellow text-black rounded px-6 py-3 mb-10">
-              <Link href="/reserve">Reserve a Table</Link>
-            </Button>
-          </div>
+        <div className="grid grid-cols-4 md:grid-cols-12 items-center">
+          <div className="col-span-4 md:col-start-1 md:col-span-12 flex flex-col md:justify-evenly md:flex-row gap-5 text-white">
+            <div>
+              <h1 className={`text-56 ${markaziText.className} font-bold`}>
+                Little Lemon
+              </h1>
+              <span className={`text-32 ${markaziText.className}`}>
+                New York
+              </span>
+              <p
+                className={`max-w-2xl md:max-w-lg text-20 ${karla.className} mb-5`}
+              >
+                Experience culinary bliss at Little Lemon, where taste meets
+                artistry. Immerse yourself in a world of exquisite flavors,
+                carefully crafted from the finest ingredients.
+              </p>
+              <Button className="text-18 bg-primary-yellow text-black rounded px-6 py-3 mb-10">
+                <Link href="/reserve">Reserve a Table</Link>
+              </Button>
+            </div>
 
-          <div className="self-center">
-            <Image
-              src={HeaderImage}
-              alt="restaurant food being served"
-              width={300}
-              className="object-contain rounded-2xl"
-            />
+            <div className="self-center">
+              <Image
+                src={HeaderImage}
+                alt="restaurant food being served"
+                width={300}
+                className="object-contain rounded-2xl"
+              />
+            </div>
           </div>
         </div>
       </div>
