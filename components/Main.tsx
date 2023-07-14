06eb4793df67b21bf0fa68f2926936fa4ca65eec
dev-rome <haynesjerome@gmail.com>
2023-07-14 16:52:00 -0400

    imported specials to main

--- components/Main.tsx
+++ components/Main.tsx
@@ -2,10 +2,15 @@ import React from "react";
 
 import Header from "./layout/Header";
 
+import Specials from "./sections/Specials"
+import Testimonials from "./sections/Testimonials"
+import About from "./sections/About"
+
 const Main = () => {
   return (
     <>
       <Header />
+      <Specials />
     </>
   );
 };
