a15224c1170bb3ce85dddda999971db0213ffeac
dev-rome <haynesjerome@gmail.com>
2023-07-14 16:49:34 -0400

    add card to specials to display specials and edit imports

--- components/sections/Specials.tsx
+++ components/sections/Specials.tsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import { RiEBike2Fill } from "react-icons/ri";
+
+import { markaziText } from "@/app/fonts";
+import { specialsData } from "@/constants/specials";
+import { CardProps } from "@/types";
+
+const Card = ({ special }: CardProps) => {
+  const { image, title, price, description } = special;
+
+  return (
+    <div className="bg-white rounded-lg shadow-md overflow-hidden flex flex-col justify-between">
+      <div className="relative h-48">
+        <Image src={image} alt={title} layout="fill" objectFit="cover" />
+      </div>
+      <div className="p-6">
+        <div className="flex justify-between items-center mb-9">
+          <h3 className={`text-24 font-medium ${markaziText.className}`}>
+            {title}
+          </h3>
+          <span className="font-bold text-secondary-lightPeach">{price}</span>
+        </div>
+        <p className="mb-4">{description}</p>
+      </div>
+      <div className="p-6 mt-auto">
+        <button className="text-16 font-bold bg-none text-black">
+          Order a delivery
+          <RiEBike2Fill className="inline-block ml-2" />
+        </button>
+      </div>
+    </div>
+  );
+};
 
 const Specials = () => {
   return (
-    <div>Specials</div>
-  )
-}
+    <section className="py-24">
+      <div className="max-w-[80rem] mx-auto px-4">
+        <div className="grid grid-cols-4 md:grid-cols-12">
+          <div className="col-span-4 md:col-start-1 md:col-span-12 flex justify-between items-center mb-11">
+            <h2 className="text-40 font-bold">Specials</h2>
+            <button className="text-18 bg-primary-yellow text-black rounded px-6 py-3">
+              Online Menu
+            </button>
+          </div>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {specialsData.map((special) => (
+            <Card key={special.id} special={special} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Specials
+export default Specials;
