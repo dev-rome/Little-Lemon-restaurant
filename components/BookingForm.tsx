7154caab619dd650307af5c73ec97201b1c3870e
dev-rome <haynesjerome@gmail.com>
2023-07-15 12:30:00 -0400

    change button color and added cursor-pointer

--- components/BookingForm.tsx
+++ components/BookingForm.tsx
@@ -35,7 +35,7 @@ const BookingForm = () => {
       <input
         type="submit"
         value="Make Your reservation"
-        className="bg-blue-500 text-white px-4 py-2 rounded-md"
+        className="bg-primary-darkGreen text-white px-4 py-2 rounded-md cursor-pointer"
       />
     </form>
   );
