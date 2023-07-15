f3465fedd072646a13a949fc965e1f0c43e299d1
dev-rome <haynesjerome@gmail.com>
2023-07-15 12:03:53 -0400

    add socialMedia data

---
+++ constants/socialLinks.ts
@@ -0,0 +1,7 @@
+import { BsFacebook, BsInstagram, BsSnapchat } from "react-icons/bs";
+
+export const socialMediaIcons = [
+    { icon: BsFacebook, href: "/" },
+    { icon: BsInstagram, href: "/" },
+    { icon: BsSnapchat, href: "/" },
+  ];
