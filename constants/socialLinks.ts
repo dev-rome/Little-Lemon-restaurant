fc0a4cbc280cfb020b438fa3c59475dfe1a781d1
dev-rome <haynesjerome@gmail.com>
2023-07-15 16:47:02 -0400

    add name to data

--- constants/socialLinks.ts
+++ constants/socialLinks.ts
@@ -1,7 +1,7 @@
 import { BsFacebook, BsInstagram, BsSnapchat } from "react-icons/bs";
 
 export const socialMediaIcons = [
-    { icon: BsFacebook, href: "/" },
-    { icon: BsInstagram, href: "/" },
-    { icon: BsSnapchat, href: "/" },
-  ];
+  { name: "Facebook", icon: BsFacebook, href: "/" },
+  { name: "Instagram", icon: BsInstagram, href: "/" },
+  { name: "SnapChat", icon: BsSnapchat, href: "/" },
+];
