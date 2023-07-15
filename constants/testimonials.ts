905a4cceb893b642a5e032f338cb3f61aa6ed71a
dev-rome <haynesjerome@gmail.com>
2023-07-14 21:55:09 -0400

    added testimonial data

---
+++ constants/testimonials.ts
@@ -0,0 +1,34 @@
+export const testimonialData = [
+  {
+    id: 1,
+    rating: 5,
+    image: "/images/user1.jpg",
+    name: "Emily Johnson",
+    comment:
+      "I had a great experience at this restaurant. The food was amazing, and the service was outstanding. Highly recommended!",
+  },
+  {
+    id: 2,
+    rating: 4,
+    image: "/images/user2.jpg",
+    name: "Ethan Davis",
+    comment:
+      "The dining experience was fantastic! The dishes were exquisite, and the staff provided exceptional service. I would definitely go again!",
+  },
+  {
+    id: 3,
+    rating: 4.5,
+    image: "/images/user3.jpg",
+    name: "Liam Wilson",
+    comment:
+      "The food was exceptional, and the atmosphere was lovely. The staff was attentive and made us feel welcome. Highly recommended!",
+  },
+  {
+    id: 4,
+    rating: 5,
+    image: "/images/user4.jpg",
+    name: "Olivia Thompson",
+    comment:
+      "I had an amazing time at this restaurant. The food was outstanding, and the service was impeccable. Will definitely come back!",
+  },
+];
