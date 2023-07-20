fa3a90114666467f28a451b5fecd868181e42be5
dev-rome <haynesjerome@gmail.com>
2023-07-20 13:19:33 -0400

    add aria label to button

--- components/Button.tsx
+++ components/Button.tsx
@@ -2,13 +2,14 @@ import React from "react";
 
 import { ButtonProps } from "@/types";
 
-const Button = ({
-  children,
-  className,
-  ...rest
-}: ButtonProps) => {
+const Button = ({ children, className, ariaLabel, ...rest }: ButtonProps) => {
   return (
-    <button className={className} type="button" {...rest}>
+    <button
+      className={className}
+      type="button"
+      {...rest}
+      aria-label={ariaLabel}
+    >
       {children}
     </button>
   );
