52403aaba408fdffd0d1f6e0cb2399623c68f9e9
dev-rome <haynesjerome@gmail.com>
2023-07-20 14:56:23 -0400

    change imports

--- components/sections/About.tsx
+++ components/sections/About.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 
-import Restaurant from "@/public/images/restaurant.jpg";
+import Restaurant from "@/images/restaurant.jpg";
 import { markaziText, karla } from "@/app/fonts";
 
 const About = () => {
