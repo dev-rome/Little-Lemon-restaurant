4ebf6a8bced574b0a2fb7c40e8ad1c9320cc5ebd
dev-rome <haynesjerome@gmail.com>
2023-07-20 14:37:44 -0400

    change all imports

--- components/layout/Nav.tsx
+++ components/layout/Nav.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from "react";
 import Image from "next/image";
-import Logo from "@/public/images/logo.svg";
+import Logo from "../public/images/logo.svg";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 import NavLinks from "./NavLinks";
