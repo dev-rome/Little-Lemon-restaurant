739041c56bf551ba7b9ebceaee20a05d98e52e0b
dev-rome <haynesjerome@gmail.com>
2023-07-20 15:43:27 -0400

    add booking to booking-page

--- app/booking-page/page.tsx
+++ app/booking-page/page.tsx
@@ -1,43 +1,5 @@
-"use client";
-
-import React, { useReducer } from "react";
-import { useRouter } from "next/navigation";
-
-import BookingForm from "@/components/BookingForm";
-import { fetchAPI, submitAPI } from "../../utils/utils";
-
-export const initializeState = (): string[] => {
-  return fetchAPI(new Date());
-};
-
-export const updateTimes = (state: string[], action: any) => {
-  switch (action.type) {
-    case "INITIALIZE":
-      return initializeState();
-    case "GET DATE":
-      return fetchAPI(action.data);
-    default:
-      return state;
-  }
-};
+import Booking from "@/components/Booking";
 
 export default function BookingPage() {
-  const [availableTimes, dispatch] = useReducer(updateTimes, initializeState());
-
-  const router = useRouter();
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (submitAPI(e.target)) {
-      router.push("/confirm-booking");
-    }
-  };
-
-  return (
-    <BookingForm
-      availableTimes={availableTimes}
-      dispatch={dispatch}
-      handleSubmit={handleSubmit}
-    />
-  );
+  return <Booking />;
 }
